31c43d783343518165adee5a6354471e319ca381
Santiago Seira <sseira@stanford.edu>
2016-04-25 12:46:49 -0700

    initial contact

---
+++ SFMap.js
@@ -0,0 +1,22 @@
+// Set up size
+var width = 750,
+	height = width;
+
+// Set up projection that map is using
+var projection = d3.geo.mercator()
+	.center([-122.433701, 37.767683]) // San Francisco, roughly
+	.scale(225000)
+	.translate([width / 2, height / 2]);
+// This is the mapping between <longitude, latitude> position to <x, y> pixel position on the map
+// projection([lon, lat]) returns [x, y]
+
+// Add an svg element to the DOM
+var svg = d3.select("...").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+// Add svg map at correct size, assumes map is saved in a subdirectory called "data"
+svg.append("image")
+          .attr("width", width)
+          .attr("height", height)
+          .attr("xlink:href", "data/sf-map.svg");
